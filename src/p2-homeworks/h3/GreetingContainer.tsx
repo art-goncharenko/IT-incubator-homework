19740607512573dfe28f839db05607cda32250e4
art-goncharenko <goncharenko.art@gmail.com>
2022-07-09 16:29:32 -0400

    init

---
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -0,0 +1,41 @@
+import React, {ChangeEvent, useState} from 'react'
+import Greeting from './Greeting'
+import {UserType} from "./HW3";
+
+type GreetingContainerPropsType = {
+    users: UserType[]
+    addUserCallback: (user: string) => void
+}
+
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
+    const [name, setName] = useState<string>('')
+    const [error, setError] = useState<string>('')
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
+    const addUser = () => {
+        if (name !== ('')) {
+            addUserCallback(name)
+            alert(`Hello  ${name}!`)
+            setError('')
+            setName('')
+        } else {
+            setError('Name is required')
+        }
+    }
+
+    const totalUsers = users.length
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            error={error}
+            totalUsers={totalUsers}
+        />
+    )
+}
+
+export default GreetingContainer
