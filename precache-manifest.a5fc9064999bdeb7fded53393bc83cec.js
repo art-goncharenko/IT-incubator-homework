c854e6926dd99518f2ef7df117621ddc314726b4
art_goncharenko <goncharenko.art@gmail.com>
2022-08-16 21:22:17 -0400

    Updates

---
+++ precache-manifest.a5fc9064999bdeb7fded53393bc83cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332249fa4c47a1e0a3cf0888a541dd0c",
+    "url": "/IT-incubator-homework/index.html"
+  },
+  {
+    "revision": "bd6c99efefd09639e7ae",
+    "url": "/IT-incubator-homework/static/css/main.b2480c65.chunk.css"
+  },
+  {
+    "revision": "afa8ef57141f09219bbe",
+    "url": "/IT-incubator-homework/static/js/2.b4252b00.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/IT-incubator-homework/static/js/2.b4252b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6c99efefd09639e7ae",
+    "url": "/IT-incubator-homework/static/js/main.05018b56.chunk.js"
+  },
+  {
+    "revision": "b8f90d429d97cb955697",
+    "url": "/IT-incubator-homework/static/js/runtime-main.eb31292b.js"
+  }
+]);
